df39336c77aa562367f92367f456133830d9fd65
Umindu Dinal <148680984+umindudinal@users.noreply.github.com>
2024-06-06 23:29:45 +0530

    Create a new file

---
+++ sctipt.js
@@ -0,0 +1,20 @@
+const bars = document.querySelector(".bar"),
+close = document.querySelector(".close"),
+menu = document.querySelector(".menu");
+
+bars.addEventListener("click", () => {
+    menu.classList.add("active");
+    gsap.from(".menu", {
+        opacity: 0,
+        duration: .3
+    })
+
+    gsap.from(".menu ul", {
+        opacity: 0,
+        x: -300
+    })
+});
+
+close.addEventListener("click", () => {
+    menu.classList.remove("active")
+});
